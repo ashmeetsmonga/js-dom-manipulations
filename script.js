26cc6893e8c6f60d31838459d63ee66074a72a9b
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-16 20:51:56 +0530

    getElementByClassName and getElementByTagName

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ console.log(document);
 const pageBanner = document.getElementById("page-banner");
 console.log(pageBanner);
 const bookList = document.getElementById("book-list");
+
+// Class
+const titles = document.getElementsByClassName("title");
+Array.from(titles).forEach((title) => console.log(title));
+
+//Tag
+const lis = document.getElementsByTagName("li");
+console.log(lis);
