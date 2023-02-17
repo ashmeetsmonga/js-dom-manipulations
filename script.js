66ed923b9bb6cbce4ef67d7c697d39beb120a58a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-17 12:22:58 +0530

    nodes, nodeType, nodeName, node.hasChildNodes(), cloneNode

--- script.js
+++ script.js
@@ -15,4 +15,13 @@ const wrapper = document.querySelector("#wrapper");
 const wmf = document.querySelector("#book-list li:nth-child(2) .name");
 
 const book = document.querySelectorAll("#book-list li .name");
-book.forEach((b) => console.log(b));
+// book.forEach((b) => console.log(b));
+
+//Nodes
+const banner = document.querySelector("#page-banner");
+console.log(banner.nodeType);
+console.log(banner.nodeName);
+console.log(banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(true); //true means insides are also cloned
+console.log(clonedBanner);
